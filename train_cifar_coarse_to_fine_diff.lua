1f00e0e1a555835bd39f3e93e3de7ac6c0b47a2c
Soumith Chintala <soumith@fb.com>
2015-05-05 23:25:51 -0400

    default float tensor

--- train_cifar_coarse_to_fine_diff.lua
+++ train_cifar_coarse_to_fine_diff.lua
@@ -20,7 +20,6 @@ opt = lapp[[
   -s,--save          (default "logs")      subdirectory to save logs
   --saveFreq         (default 10)          save every saveFreq epochs
   -n,--network       (default "")          reload pretrained network
-  -f,--full          (default true)        use the full dataset
   -p,--plot                                plot while training
   -r,--learningRate  (default 0.01)        learning rate, for SGD only
   -b,--batchSize     (default 100)          batch size
@@ -46,8 +45,6 @@ print('<torch> set nb of threads to ' .. torch.getnumthreads())
 if opt.gpu then
   cutorch.setDevice(opt.gpu + 1)
   print('<gpu> using device ' .. opt.gpu)
-  torch.setdefaulttensortype('torch.CudaTensor')
-else
   torch.setdefaulttensortype('torch.FloatTensor')
 end
 
@@ -123,34 +120,19 @@ print(model_D)
 print('Generator network:')
 print(model_G)
 
-local nparams = 0
-for i=1,#model_D.modules do
-  if model_D.modules[i].weight ~= nil then
-    nparams = nparams + model_D.modules[i].weight:nElement()
-  end
+local freeParams = function(m)
+   local list = m:listModules()
+   local p = 0
+   for k,v in pairs(list) do
+      p = p + v.weight and v.weight:nElement() or 0
+      p = p + v.bias and v.bias:nElement() or 0
+   end
+   return p
 end
-print('\nNumber of free parameters in D: ' .. nparams)
- 
-local nparams = 0
-for i=1,#model_G.modules do
-  if model_G.modules[i].weight ~= nil then
-    nparams = nparams + model_G.modules[i].weight:nElement()
-  end
-end
-print('Number of free parameters in G: ' .. nparams .. '\n')
+print('\nNumber of free parameters in D: ' .. freeParams(model_D)) 
+print('Number of free parameters in G: ' .. freeParams(model_G) .. '\n')
 
 ----------------------------------------------------------------------
--- get/create dataset
---
-if opt.full then
-  ntrain = 45000
-  nval = 5000
-else
-  ntrain = 2000
-  nval = 1000
-  print('<warning> only using 2000 samples to train quickly (use flag -full to use 60000 samples)')
-end
-
 -- create training set and normalize
 trainData = cifar.loadTrainSet(1, ntrain)
 --image_utils.contrastNormalize(trainData.data, -1, 1)
@@ -172,19 +154,8 @@ valData:makeCoarse()
 valData:makeDiff()
 torch.setdefaulttensortype(defaultType)
 
--- create test set and normalize
---[[
-testData = cifar.loadTestSet()
-image_utils.normalize(testData.fineData, fineMean, fineStd)
-image_utils.normalize(testData.coarseData, coarseMean, coarseStd)
---]]
-
 -- this matrix records the current confusion across classes
 confusion = optim.ConfusionMatrix(classes)
-
--- log results to files
-trainLogger = optim.Logger(paths.concat(opt.save, 'train.log'))
-testLogger = optim.Logger(paths.concat(opt.save, 'test.log'))
 
 if opt.gpu then
   print('Copy model to gpu')
@@ -233,12 +204,6 @@ function getSamples(dataset, N)
   return to_plot
 end
 
---[[
-local to_plot = getSamples(valData, 16) 
-torch.setdefaulttensortype('torch.FloatTensor')
-disp.image(to_plot, {win=opt.window, width=600})
-debugger.enter()
---]]
 -- training loop
 while true do
   -- train/test
@@ -253,18 +218,6 @@ while true do
   -- plot errors
   if opt.plot  and epoch and epoch % 1 == 0 then
     local to_plot = getSamples(valData, 16) 
-    torch.setdefaulttensortype('torch.FloatTensor')
-
-    trainLogger:style{['% mean class accuracy (train set)'] = '-'}
-    testLogger:style{['% mean class accuracy (test set)'] = '-'}
-    trainLogger:plot()
-    testLogger:plot()
-
     disp.image(to_plot, {win=opt.window, width=600})
-    if opt.gpu then
-      torch.setdefaulttensortype('torch.CudaTensor')
-    else
-      torch.setdefaulttensortype('torch.FloatTensor')
-    end
   end
 end
