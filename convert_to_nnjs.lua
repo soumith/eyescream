c485aba24c9c442282ce1cbf248f767af0404939
Soumith Chintala <soumith@gmail.com>
2015-06-21 02:46:24 -0400

    making msgpack the default

--- convert_to_nnjs.lua
+++ convert_to_nnjs.lua
@@ -31,7 +31,7 @@ function convertToNNJS(mod)
       out.type = 'ReLU'
    elseif torch.type(mod) == 'nn.View' then
       out.type = 'View'
-      out.dims = m[1].modules[7].size:totable()
+      out.dims = mod.size:totable()
    elseif torch.type(mod) == 'nn.ParallelTable' then
       out.type = 'ParallelTable'
       out.modules = {}
@@ -42,7 +42,7 @@ function convertToNNJS(mod)
       out.type = 'Identity'
    elseif torch.type(mod) == 'nn.SpatialConvolution' then
       out.type = 'SpatialConvolution'
-      out.weight = mod.weight:transpose(2,4):transpose(2,3):contiguous():storage():totable()
+      out.weight = mod.weight:storage():totable()
       out.bias =  mod.bias:storage():totable()
       out.nOutputPlane = mod.nOutputPlane
       out.nInputPlane = mod.nInputPlane
@@ -64,10 +64,21 @@ f = io.open('8x8.json', 'w')
 f:write(json.encode(enc))
 f:close()
 
+torch.manualSeed(1)
+input = {torch.randn(1,100), torch.randn(1,10)}
+out = m[1]:forward(input)
+ioo = {}
+ioo.input = {input[1]:storage():totable(), input[2]:storage():totable()}
+ioo.output = out:storage():totable()
+
+f = io.open('8x8.out.json', 'w')
+f:write(json.encode(ioo))
+f:close()
+
 f = io.open('8x8.mpac', 'w')
 f:write(mp.pack(enc))
 f:close()
-os.execute('gzip 8x8.mpac')
+-- os.execute('gzip 8x8.mpac')
 print('saved json and mpac for 8x8')
 ---------------------
 print(m[2])
@@ -78,10 +89,27 @@ f = io.open('8x14.json', 'w')
 f:write(json.encode(enc))
 f:close()
 
+torch.manualSeed(1)
+input = {torch.zeros(1,1,14,14):uniform(-1,1),
+	 torch.randn(1,10,1,1),
+	 torch.randn(1,3,14,14)}
+out = m[2]:forward(input)
+ioo = {}
+ioo.input = {
+   input[1]:storage():totable(),
+   input[2]:storage():totable(),
+   input[3]:storage():totable()
+}
+ioo.output = out:storage():totable()
+
+f = io.open('8x14.out.json', 'w')
+f:write(json.encode(ioo))
+f:close()
+
 f = io.open('8x14.mpac', 'w')
 f:write(mp.pack(enc))
 f:close()
-os.execute('gzip 8x14.mpac')
+-- os.execute('gzip 8x14.mpac')
 print('saved json and mpac for 8x14')
 ---------------------------------
 print(m[3])
@@ -92,8 +120,26 @@ f = io.open('14x28.json', 'w')
 f:write(json.encode(enc))
 f:close()
 
+torch.manualSeed(1)
+input = {torch.zeros(1,1,28,28):uniform(-1,1),
+	 torch.randn(1,10,1,1),
+	 torch.randn(1,3,28,28)}
+out = m[3]:forward(input)
+ioo = {}
+ioo.input = {
+   input[1]:storage():totable(),
+   input[2]:storage():totable(),
+   input[3]:storage():totable()
+}
+ioo.output = out:storage():totable()
+
+f = io.open('14x28.out.json', 'w')
+f:write(json.encode(ioo))
+f:close()
+
+
 f = io.open('14x28.mpac', 'w')
 f:write(mp.pack(enc))
 f:close()
-os.execute('gzip 14x28.mpac')
+-- os.execute('gzip 14x28.mpac')
 print('saved json and mpac for 14x28')
